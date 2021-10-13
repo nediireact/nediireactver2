266f342877d3fc16f3db45bf71ed3fdcbee58aea
Christopher Guzman <christopher.guzman.monsalvo@gmail.com>
2021-10-13 02:05:42 +0000

    Add: docker

--- src/constants/EnvironmentVariables.ts
+++ src/constants/EnvironmentVariables.ts
@@ -4,8 +4,8 @@ const {
   REACT_APP_API_URL,
   REACT_APP_IS_MOBILE_APP,
   REACT_APP_BRANCH_NAME,
-  REACT_APP_PRODUCTION,
-  REACT_APP_FACEBOOK_APP_ID
+  REACT_APP_FACEBOOK_APP_ID,
+  REACT_APP_ENVT
 } = process.env;
 
 class EnvironmentVariables {
@@ -19,10 +19,10 @@ class EnvironmentVariables {
 
   constructor() {
     this._apiBaseUrl = REACT_APP_API_URL ?
-      REACT_APP_API_URL : 'https://api.nedii.iguzman.com.mx/v1/';
+      REACT_APP_API_URL : 'https://api.nedii.com/v1/';
     this._isMobileApp = REACT_APP_IS_MOBILE_APP ? true : false;
-    this._production = REACT_APP_PRODUCTION ? true : false;
     this._branchName = REACT_APP_BRANCH_NAME ? REACT_APP_BRANCH_NAME : 'local-branch';
+    this._production = REACT_APP_ENVT === 'master' ? true : false;
     this._version = packageJSON ? packageJSON.version : '0.0.0';
     this._facebookAppID = REACT_APP_FACEBOOK_APP_ID ? REACT_APP_FACEBOOK_APP_ID : '-';
   }
