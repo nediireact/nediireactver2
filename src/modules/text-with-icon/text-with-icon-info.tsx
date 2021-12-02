4923066f4c8432485159e40e7715b81d0828aeea
edgar <e.gm27@outlook.com>
2021-12-02 05:10:48 +0000

    Cambios en detalle de comida

---
+++ src/modules/text-with-icon/text-with-icon-info.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'src/modules/text-with-icon/text-with-icon.scss';
+
+const TextWhitIconInfo = (props: any): React.ReactElement => {
+
+  return (
+  <div className={`TextWithIconinfo ${props.className}`}>
+    <i className={`material-icons ${props.colorIcon}`}>{props.icon}</i>
+    <span className='grey-text text-darken-4'>{props.text}</span>
+  </div>
+  );
+};
+
+export default TextWhitIconInfo;
